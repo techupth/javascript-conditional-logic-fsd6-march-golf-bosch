0e9ccadbfdfc3c8406f0445d7bb41e98de556089
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-05-15 13:32:43 +0700

    feat: ex if-else

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,16 @@
 //Exercise 3
 // Ternary Operator
 let lightBulbStatus = "Off";
+let message;
 
 //Start coding here
+message =
+  lightBulbStatus === "On"
+    ? "Light bulb is On."
+    : "Please choose the correct input (On/Off)";
+message =
+  lightBulbStatus === "Off"
+    ? "Light bulb is Off."
+    : "Please choose the correct input (On/Off)";
+
+console.log(message);
--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,20 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+switch (lightBulbStatus) {
+  case (lightBulbStatus = "On"):
+    console.log("Light bulb is On.");
+    break;
+
+  case (lightBulbStatus = "Off"):
+    console.log("Light bulb is Off.");
+    break;
+
+  case (lightBulbStatus = "Broken"):
+    console.log("Light bulb is Broken.");
+    break;
+
+  default:
+    console.log("Please choose the correct input (On/Off/Broken)");
+    break;
+}
